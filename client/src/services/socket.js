fd9d7d08037801f747142c7fd5561c8335d3b91c
Keyprd <140514151+Keyprd@users.noreply.github.com>
2026-03-20 11:55:49 +0800

    wd

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001", {
+export const socket = io("https://agritrust.shop", {
   autoConnect: false, // connect manually
 });
